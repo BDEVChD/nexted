7c3adc9593127835c2a3d9278707bac2b2a2600b
Bladi Duran <bladi.duran@gmail.com>
2021-03-17 17:49:20 -0400

    added react

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -4,10 +4,21 @@ import {useState, useEffect} from 'react';
 
 
 
-export default function About() {
-  useEffect(() => {
-    console.log('component loaded')
-  }, [])
+export default function About({data}) {
+  const [state, setState ] = useState({
+    name: data.name
+  })
+  // useEffect(() => {
+  //   console.log('component loaded')
+  //   axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+  //   .then((response)=> {
+  //     console.log(response)
+  //     setState({
+  //       name: response.data.name,
+  //       img: response.data.sprites.front_default
+  //     })
+  //   })
+  // }, [])
   return (
     <div >
       <Head>
@@ -17,7 +28,39 @@ export default function About() {
       </Head>
     <h1>Pokemon</h1>
     <p>This is the about page</p>
-     
+  <h1>{state.name}</h1>
+
     </div>
   )
+}
+/** STATIC SITE GENERATION
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+  const pokemon = await res.json(); 
+  //   axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+  //   .then((response)=> {
+  //     console.log(response)
+  //     setState({
+  //       name: response.data.name,
+  //       img: response.data.sprites.front_default
+  //     })
+  //   })
+  // }, [])
+  return {
+    props: {
+      pokemon
+    },
+  }
+}
+ */
+
+ //Server Side 
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
